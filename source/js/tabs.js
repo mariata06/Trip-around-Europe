147e27f5e2e35468bbdc758987f13564eda41f5c
Мария <slinkywizard@mail.ru>
2020-08-22 23:17:29 +0300

    Сверстала страницу в формате десктоп, добавила скрипт переключения табов

---
+++ source/js/tabs.js
@@ -0,0 +1,36 @@
+var tabItem = function () {
+  var tabMenu = document.querySelectorAll(".places__choice-country");
+  var tabContent = document.querySelectorAll(".countries__card");
+  var tabName;
+  var countryCards = document.querySelectorAll(".countries__item");
+
+  tabMenu.forEach(function (item) {
+    item.addEventListener('click', toggleTabItem)
+  })
+
+  countryCards.forEach(function (item) {
+    item.addEventListener('click', toggleTabItem)
+  })
+
+
+  function toggleTabItem() {
+    tabMenu.forEach(function (item) {
+      item.classList.remove("places__choice-country--active")
+    });
+    this.classList.add("places__choice-country--active");
+    tabName = this.getAttribute("data-tab-name");
+    selectTabContent(tabName);
+  }
+
+  function selectTabContent(tabName) {
+    tabContent.forEach(function (item) {
+      if (item.classList.contains(tabName)) {
+        item.classList.add("countries__card--active")
+      } else {
+        item.classList.remove("countries__card--active");
+      }
+    })
+  }
+};
+
+tabItem();
